da6584a45025add4a0acaadbe9cb0ad88485ce78
Ole Martin Handeland <git@olemartin.org>
2016-01-10 17:11:58 +0100

    Issue #7: Added command-line options to dump pid to a file, so that it can be paused and resumed

--- alternating_layouts.py
+++ alternating_layouts.py
@@ -3,6 +3,9 @@
 import i3
 import re
 import subprocess
+import getopt
+import sys
+import os
 
 
 def find_parent(window_id):
@@ -48,12 +51,34 @@ def set_layout():
             i3.split(new_layout)
 
 
+def print_help():
+    print("Usage: " + sys.argv[0] + " [-p path/to/pid.file]")
+    print("")
+    print("Options:")
+    print("    -p path/to/pid.file   Saves the PID for this program in the filename specified")
+    print("")
+
+
 def main():
     """
         Main function - listen for window focus
         changes and call set_layout when focus
         changes
     """
+    opt_list, args = getopt.getopt(sys.argv[1:], 'hp:')
+    pid_file = None
+    for opt in opt_list:
+        if opt[0] == "-h":
+            print_help()
+            sys.exit()
+        if opt[0] == "-p":
+            pid_file = opt[1]
+
+    if pid_file:
+        with open(pid_file, 'w') as f:
+            f.write(str(os.getpid()))
+
+
     process = subprocess.Popen(
         ['xprop', '-root', '-spy'],
         stdout=subprocess.PIPE,
